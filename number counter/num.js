ea0241a672befba11859801cc7f14f211ed6516b
sahani kumar <146368331+sahani-kumar@users.noreply.github.com>
2024-12-28 18:02:33 +0000

    updated

--- number counter/num.js
+++ number counter/num.js
@@ -10,10 +10,16 @@ increasebtn.onclick = function(){
     count++;
     label.textContent = count;
 } 
+
+// for simple reset
+
 resetbtn.onclick = function(){
     count=0;
     label.textContent = count;
 }
+
+// for simple decrease
+
 decreasebtn.onclick = function(){
     count--;
     label.textContent = count;
